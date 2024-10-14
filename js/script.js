db43c3b023fd06844b50af6f8b91643d403524eb
tamar <tamarlouf1@gmail.com>
2024-10-14 14:14:39 +0300

    forms& api

---
+++ js/script.js
@@ -0,0 +1,308 @@
+import data from "../books.json" with { type: "json" };
+
+document.addEventListener('DOMContentLoaded', function () {
+    const productsContainer = document.getElementById('products-container');
+    const addBookButton = document.createElement('button');
+    addBookButton.innerText = 'הוסף ספר חדש';
+    addBookButton.id = 'add-book-button';
+    productsContainer.appendChild(addBookButton);
+    initialBooks();
+
+    let sortOrder = {
+        id: 'none',
+        title: 'none',
+        price: 'none',
+        rate: 'none'
+    };
+
+    addBookButton.addEventListener('click', function () {
+        showEditForm();
+    });
+
+    function getSortArrow(field) {
+        if (sortOrder[field] === 'asc') {
+            return ' ↑';
+        } else if (sortOrder[field] === 'desc') {
+            return ' ↓';
+        } else {
+            return '';
+        }
+    }
+
+    function renderBooks() {
+        const books = getAllBooks();
+        productsContainer.innerHTML = '';
+        productsContainer.appendChild(addBookButton);
+
+        const headerRow = document.createElement('div');
+        headerRow.classList.add('table-header');
+        headerRow.innerHTML = `
+            <span id="id-header" class="sortable">אינדקס${getSortArrow('id')}</span>
+            <span id="title-header" class="sortable">כותרת${getSortArrow('title')}</span>
+            <span id="price-header" class="sortable">מחיר${getSortArrow('price')}</span>
+            <span id="rate-header" class="sortable">דירוג${getSortArrow('rate')}</span>
+        `;
+        productsContainer.appendChild(headerRow);
+
+        books.forEach(book => {
+            const bookRow = document.createElement('div');
+            bookRow.classList.add('book-row');
+            bookRow.innerHTML = `
+                <p class ="book-id">${book.id}<p>
+                <h3 class="book-title">${book.title}</h3>
+                <p class="book-price">$${book.price.toFixed(2)}</p>
+                <p class="book-rate">rating: ${book.rate}</p>
+                <button class="read-book" data-id="${book.id}">צפיה</button>
+                <button class="edit-book" data-id="${book.id}">ערוך</button>
+                <button class="remove-book" data-id="${book.id}">מחק</button>
+            `;
+            productsContainer.appendChild(bookRow);
+        });
+
+        document.querySelectorAll('.remove-book').forEach(button => {
+            button.addEventListener('click', function () {
+                const id = parseInt(this.getAttribute('data-id'));
+                removeBook(id);
+            });
+        });
+
+        document.querySelectorAll('.edit-book').forEach(button => {
+            button.addEventListener('click', function () {
+                const id = parseInt(this.getAttribute('data-id'));
+                const book = getById(id);
+                showEditForm(book);
+                console.log(book);
+
+            });
+        });
+        document.querySelectorAll('.read-book').forEach(button => {
+            button.addEventListener('click', function () {
+                const id = parseInt(this.getAttribute('data-id'));
+                const book = getById(id)
+                showReadBook(book)
+            })
+        })
+
+        document.getElementById('id-header').addEventListener('click', () => sortBooks('id'));
+        document.getElementById('title-header').addEventListener('click', () => sortBooks('title'));
+        document.getElementById('price-header').addEventListener('click', () => sortBooks('price'));
+        document.getElementById('rate-header').addEventListener('click', () => sortBooks('rate'));
+    }
+    //לכתוב טופס הצגת ספר + דירוג
+    
+    // let lastAssignedId = 40;
+
+    // function getNextId() {
+    //     return lastAssignedId++;
+    // }
+
+    function showEditForm(book = null) {
+        const overlay = document.createElement('div');
+        overlay.id = 'overlay';
+        document.body.appendChild(overlay);
+
+        const editForm = document.createElement('div');
+        let newId;
+        if (book) {
+            newId = book.id;
+        } else {
+            const books = getAllBooks();
+            newId = books.length ? books[books.length - 1].id + 1 : 1;
+        }
+        editForm.innerHTML = `
+            <h2>${book ? 'ערוך ספר' : 'הוסף ספר חדש'}</h2>
+            
+            <div class="form-field">
+                <label for="edit-id">אינדקס:</label>
+                <input type="number" id="edit-id" value="${newId}" required readonly>
+            </div>
+            <div class="form-field">
+                <label for="edit-title">כותרת:</label>
+                <input type="text" id="edit-title" value="${book ? book.title : ''}" required>
+            </div>
+            <div class="form-field">
+                <label for="edit-img">תמונה:</label>
+                <input type="text" id="edit-img" value="${book ? book.coverImageUrl : ''}" required>
+            </div>
+            <div class="form-field">
+                <label for="edit-price">מחיר:</label>
+                <input type="number" id="edit-price" value="${book ? book.price : ''}" required>
+            </div>
+            <div class="form-field">
+                <label for="edit-rate">דירוג:</label>
+                <input type="range" id="edit-rate" value="${book ? book.rate : '0'}" step="0.1" min="0" max="5" required>
+                <span id="rate-value">${book ? book.rate : '0'}</span>
+            </div>
+            <button id="save-book">${book ? 'שמור' : 'צור'}</button>
+            <button id="cancel-edit">ביטול</button>
+        `;
+        console.log('טופס נוצר בהצלחה:', editForm);
+
+
+        if (document.getElementById('edit-form')) {
+            productsContainer.removeChild(document.getElementById('edit-form'));
+        }
+        editForm.id = 'edit-form';
+        document.body.appendChild(editForm);
+        const rateInput = document.getElementById('edit-rate');
+        const rateValueDisplay = document.getElementById('rate-value');
+
+        // הצגת הדירוג הנוכחי
+        rateValueDisplay.textContent = rateInput.value;
+
+        // עדכון התצוגה כשיש שינוי
+        rateInput.addEventListener('input', function () {
+            rateValueDisplay.textContent = this.value;
+        });
+
+        document.getElementById('save-book').addEventListener('click', function () {
+            const updatedBook = {
+                id: parseInt(document.getElementById('edit-id').value),
+                title: document.getElementById('edit-title').value,
+                img: document.getElementById('edit-img').value,
+                price: parseFloat(document.getElementById('edit-price').value),
+                rate: parseFloat(document.getElementById('edit-rate').value)
+            };
+            console.log(updateBook);
+            console.log(updateBook);
+
+
+            if (book) {
+                book.id = updatedBook.id;
+                updateBook(updatedBook);
+            } else {
+                const newBook = {
+                    id: document.getElementById('edit-id').value,
+                    title: document.getElementById('edit-title').value,
+                    img: document.getElementById('edit-img').value,
+                    price: parseFloat(document.getElementById('edit-price').value),
+                    rate: parseFloat(document.getElementById('edit-rate').value)
+                };
+                createBook(newBook);
+            }
+            closeEditForm();
+            renderBooks();
+        });
+
+        document.getElementById('cancel-edit').addEventListener('click', closeEditForm);
+        overlay.addEventListener('click', closeEditForm);
+    }
+    function closeEditForm() {
+        const editForm = document.getElementById('edit-form');
+        const overlay = document.getElementById('overlay');
+
+        if (editForm) {
+            document.body.removeChild(editForm);
+        }
+        if (overlay) {
+            document.body.removeChild(overlay);
+        }
+    }
+
+
+
+    function saveBooksToLocalStorage(books) {
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    function initialBooks() {
+        if (!localStorage.getItem('books')) {
+            saveBooksToLocalStorage(data.books);
+        }
+    }
+
+    function getAllBooks() {
+        const books = localStorage.getItem('books');
+        return books ? JSON.parse(books) : [];
+    }
+
+    function getById(id) {
+        const allBooks = getAllBooks();
+        return allBooks.find(book => book.id === id);
+    }
+
+    function removeBook(id) {
+        const allBooks = getAllBooks();
+        const updatedArrayBooks = allBooks.filter(book => book.id !== id);
+        saveBooksToLocalStorage(updatedArrayBooks);
+        renderBooks();
+    }
+    //בדיקת כפילות קוד מול הטופס לעריכת ספר/ חדש
+    function createBook(newBook) {
+        const books = getAllBooks();
+        newBook.id = books.length ? books[books.length - 1].id + 1 : 1;
+        books.push(newBook);
+        saveBooksToLocalStorage(books);
+        renderBooks();
+    }
+    function updateBook(updatedBook) {
+        let books = getAllBooks();
+
+        const index = books.findIndex(book => book.id === updatedBook.id);
+        if (index !== -1) {
+            books[index] = updatedBook;
+            saveBooksToLocalStorage(books);
+        }
+    }
+
+    function sortBooks(field) {
+        const direction = sortOrder[field] === 'asc' ? 'desc' : 'asc';
+        sortOrder[field] = direction;
+
+        const books = getAllBooks();
+        books.sort((a, b) => {
+            if (field === 'title') {
+                return direction === 'asc'
+                    ? a.title.localeCompare(b.title)
+                    : b.title.localeCompare(a.title);
+            } else if (field === 'id') {
+                return direction === 'asc' ? a.id - b.id : b.id - a.id;
+            } else if (field === 'price') {
+                return direction === 'asc' ? a.price - b.price : b.price - a.price;
+            } else if (field === 'rate') {
+                return direction === 'asc' ? a.rate - b.rate : b.rate - a.rate;
+            }
+
+        });
+
+        saveBooksToLocalStorage(books); // שמירה אחרי המיון
+        renderBooks();
+    }
+
+    createBook({
+        "title": "1984",
+        "price": 39.99,
+        "coverImageUrl": "https://upload.wikimedia.org/wikipedia/en/c/c3/1984first.jpg",
+        "rate": 4.7
+    });
+
+    console.log(getAllBooks());
+});
+
+function showReadBook(book) {
+    const previewContainer = document.createElement('div')
+    previewContainer.id = 'preview-container'
+    previewContainer.innerHTML = `
+        <h2>${book.title}</h2>
+        <img src="${book.coverImageUrl}" alt="${book.title}">
+        <p>מחיר: $${book.price.toFixed(2)}</p>
+        <div class="form-field">
+            <label for="edit-rate">דירוג:</label>
+            <input type="range" id="edit-rate" value="${book ? book.rate : '0'}" step="0.1" min="0" max="5" required>
+            <span id="rate-value">${book ? book.rate : '0'}</span>
+        </div>
+        `;
+    const bookPreviewContainer = document.getElementById('book-preview-container');
+    const existingPreview = document.getElementById('preview-container')
+
+    if (existingPreview) {
+        bookPreviewContainer.removeChild(existingPreview)
+    }
+    bookPreviewContainer.appendChild(previewContainer)
+
+}
+
+
+
+// התמונה של הספר
+/* <img src="${book.coverImageUrl}" alt="${book.title}" class="book-image book-cover"> */
